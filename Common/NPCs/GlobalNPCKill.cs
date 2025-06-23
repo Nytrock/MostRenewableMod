7d8b58d118f273cf172ff28338a83b2e178972f1
Nytrock <savvazhirnov@gmail.com>
2025-06-23 19:54:21 +0300

    Statue enemies no more drop banners

---
+++ Common/NPCs/GlobalNPCKill.cs
@@ -0,0 +1,13 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace EverythingRenewableNow.Common.NPCs {
+    public class GlobalNPCKill : GlobalNPC {
+        public override void HitEffect(NPC npc, NPC.HitInfo hit) {
+            if (npc.life > 0) return;
+            if (!npc.SpawnedFromStatue) return;
+
+            NPC.killCount[Item.NPCtoBanner(npc.BannerID())]--;
+        }
+    }
+}
